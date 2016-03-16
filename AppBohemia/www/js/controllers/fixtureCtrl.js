6021f99ef7a90338672afb78833f07737e98d4da
rfernandez <rfernandez@hexacta.com>
2016-03-15 23:56:01 -0300

    integración de fixture loading en home loading en news loading en tablas loading en fixture fix encode en titulo de noticias

--- AppBohemia/www/js/controllers/fixtureCtrl.js
+++ AppBohemia/www/js/controllers/fixtureCtrl.js
@@ -1,6 +1,31 @@
 /**
  * Created by Rod on 2/27/16.
  */
-controllers.controller('fixtureCtrl', function ($scope) {
+controllers.controller('fixtureCtrl', function ($scope, $ionicLoading, fixtureFactory) {
+  $scope.fixture = {};
+
+  $ionicLoading.show({
+    template: '<ion-spinner></ion-spinner>'
+  });
+
+  fixtureFactory.getFixture().then(function (d) {
+    $scope.fixture = d;
+    $ionicLoading.hide();
+  }, function () {
+    $ionicLoading.hide();
+  });
+
+  $scope.getPartido = function(partidos){
+    for(p in partidos){
+      var partido = partidos[p];
+      if(partido.local._id == "200" || partido.visitante._id=="200"){
+        return partido;
+      }
+    }
+  };
+
+  $scope.parseDate = function (str) {
+    return str.substring(6, 8) + '-' + str.substring(4, 6) + '-' + str.substring(0, 4);
+  };
 
 });
